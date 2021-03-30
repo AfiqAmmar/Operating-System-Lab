67c270d306d1f9217c251d00df9803180c182cce
AfiqAmmar <afiqammar0000@gmail.com>
2021-03-31 01:47:38 +0800

    Modified

--- Lab 2/LRU.py
+++ Lab 2/LRU.py
@@ -13,22 +13,22 @@ def lruPlacement(x):
             if temp[i] == block[pageFrame-1]:
                 temp[i] = reference[x]
                 break
-        print(temp)
+        print(reference[x], temp)
 
 count = 0
 for x in range(len(reference)):
     exists = reference[x] in temp
     if exists:
-        print(temp, "hit", reference[x])
+        print(reference[x], temp, "hit", reference[x])
         count += 1
     else:
         if len(temp)<pageFrame:
             temp.append(reference[x])
-            print(temp)
+            print(reference[x], temp)
         else:
             lruPlacement(x)
 
 print("Number of hit count is :", count)
 pageFault = len(reference)-count
 print("Number of page fault: ", pageFault)
-print("Success Rate: ", (count/len(reference))*100, "%")
+print("Success Rate: %.2f" %((count/len(reference))*100), "%")
