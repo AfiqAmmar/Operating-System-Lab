4e68dee210e3162c1a001dfbd68b84241a408ddc
haadifzhari <haadif23@gmail.com>
2021-03-30 18:17:22 +0800

    FIFO

---
+++ Lab 2/FIFOqueue.py
@@ -0,0 +1,38 @@
+class Queue:
+    def __init__ (self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def insertQueue(self,item):
+        self.items.insert(0,item)
+
+    def popQueue (self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def printQueue(self):
+        for items in self.items:
+            print(items," | ",end='')
+
+q = Queue()
+
+pageFrame = 3
+reference = [3,7,12,2,8,9,5,12,34,56,67]
+
+for i in range (0,len(reference)):
+    if(q.size()<pageFrame):
+        q.insertQueue(reference[i])
+    else:
+        q.popQueue()
+        q.insertQueue(reference[i])
+
+    print("time ",i,":")
+    q.printQueue()
+    print()
+
+
+
