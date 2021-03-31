9b77b92b5ae9c094409c7875fcf44ac4d2c4dd36
haadifzhari <haadif23@gmail.com>
2021-03-31 22:31:45 +0800

    FIFO

--- Lab 2/FIFOqueue.py
+++ Lab 2/FIFOqueue.py
@@ -19,15 +19,19 @@ class Queue:
             print(items," | ",end='')
 
 q = Queue()
-pageFrame = 2
-reference = [3,7,12,2,8,9,12,34,12,34,21]
+pageFrame = 3
+reference = [3,7,12,2,8,9,12]
 success = 0
+interupt = 0
 
 for i in range (0,len(reference)):
     if(q.size()<pageFrame):
         q.insertQueue(reference[i])
         success +=1
     else:
+        interupt +=1
+        print("Interrupt:",interupt)
+        print()
         q.popQueue()
         q.insertQueue(reference[i])
 
@@ -35,11 +39,13 @@ for i in range (0,len(reference)):
     print("Reference: ",reference[i])
     q.printQueue()
     print()
+    print()
 
 successrate = success/len(reference)*100
 fail = 100 - successrate
 print("Success rate is:",round(successrate,2),"%")
 print("Failure rate is:",round(fail,2),"%")
+print("Interupt:",interupt)
 
 
 
