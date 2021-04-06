e72af1a412ed45d994abc2e4b913408824bbeb9e
adamzambre <55547918+adamzambre@users.noreply.github.com>
2021-04-07 00:16:45 +0800

    Completed RoundRobinV1

--- Lab 3/Round Robin.py
+++ Lab 3/Round Robin.py
@@ -1,26 +1,39 @@
-ReadyQueue= {1:2,2:5,3:6,4:7}
-
-quantumTime=2
+ReadyQueue= {1:7,2:5,3:6,4:7}
+ReadyQueueRem= {1:7,2:5,3:6,4:7}
+WaitingTime={}
+TurnAroundTime={}
 time=0
+quantumTime=2
 
 while len(ReadyQueue)>0:
     iterator=list(ReadyQueue.keys())[0]#to get first key of the dictionary
-    
+     
     print("time "+str(time))
-    print("key "+str(iterator))
     print("job "+str(iterator)+":"+str(ReadyQueue[iterator]))
     print(ReadyQueue)
     
-    if ReadyQueue[iterator] == 2 or ReadyQueue[iterator] < quantumTime:
-        print("popping")
-        ReadyQueue.pop(iterator)
-    else:
+    if ReadyQueue[iterator] > quantumTime:
         print("moving to back of queue")
         newValue=ReadyQueue[iterator]-quantumTime
         ReadyQueue.pop(iterator)
         ReadyQueue[iterator]=newValue
- 
-    time+=1
+        time+=quantumTime
+        
+    else:
+        print("popping")
+        job=list(ReadyQueue.keys())[0]
+        time+=ReadyQueue[job]
+        WaitingTime[job]=time - ReadyQueueRem[job]
+        ReadyQueue.pop(iterator)
+
+    
     print(ReadyQueue)
     print("\n")
+
+print("jobs' waiting times: "+ str(WaitingTime))
+
+for i in range(1,len(ReadyQueueRem)+1):
+    TurnAroundTime[i]=(WaitingTime[i] - ReadyQueueRem[i])
+print("turn around time: "+str(TurnAroundTime))
+
     
