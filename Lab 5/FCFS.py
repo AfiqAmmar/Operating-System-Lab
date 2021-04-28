1f8909c3a06dbf5f7c07231a62d5c7d0b32d33a2
AfiqAmmar <afiqammar0000@gmail.com>
2021-04-28 08:35:10 +0800

    add comments

--- Lab 5/FCFS.py
+++ Lab 5/FCFS.py
@@ -1,13 +1,15 @@
 def FCFS(request, currentPosition):
-    seekTime = 0
+    seekTime = 0 # initalize seek time
     
     for i in range(0, len(request)):
-        seekTime = seekTime + abs(request[i] - currentPosition)
+        # head is set to currentPosition
+        #increment seek time by with the absolute distance of the track from the head.
+        seekTime = seekTime + abs(request[i] - currentPosition) 
         print("Current Position:", currentPosition, "Request:", request[i], "Seek Time:", seekTime)
-        currentPosition = request[i]
+        currentPosition = request[i] # set head to track
 
     print("Total Seek Time: ", seekTime)
 
-request = [176, 79, 34, 60, 92, 11, 41, 114]
-currentPosition = 48
+request = [176, 79, 34, 60, 92, 11, 41, 114] # initalize tracks
+currentPosition = 48 # initalize current position (position of the disk head)
 FCFS(request, currentPosition)
